3ecce4a9852daed450daa64668f84fb017680040
Prashant Sani <contact@prashantsani.com>
2020-05-16 01:11:13 +0530

    Updates

---
+++ precache-manifest.1a9e1bd7466b1747fa7c0ddd054c3ccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120e88022804da6a8ef38f34a6b1ddee",
+    "url": "/shopping-cart-app/index.html"
+  },
+  {
+    "revision": "815854f0df8d51fc48eb",
+    "url": "/shopping-cart-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/shopping-cart-app/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart-app/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815854f0df8d51fc48eb",
+    "url": "/shopping-cart-app/static/js/main.1b9326c0.chunk.js"
+  },
+  {
+    "revision": "93579ebc60bca0480227",
+    "url": "/shopping-cart-app/static/js/runtime-main.a418c483.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shopping-cart-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
